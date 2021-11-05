6d53ef0aeae15d642ef879108e67fd35e9704231
aadinathsukesh <85382361+aadinathsukesh@users.noreply.github.com>
2021-11-05 13:47:37 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD-C5Hf5sRAg4cFM_VD8_cd1Wbj4K_9rHs",
+    authDomain: "let-s-chat-web-app-d521a.firebaseapp.com",
+    databaseURL: "https://let-s-chat-web-app-d521a-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-web-app-d521a",
+    storageBucket: "let-s-chat-web-app-d521a.appspot.com",
+    messagingSenderId: "37688098650",
+    appId: "1:37688098650:web:845bd2d44b880b43393d63",
+    measurementId: "G-BY3VYX7FQV"
+  };
+  firebase.initialzeApp(config);
